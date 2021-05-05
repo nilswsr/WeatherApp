a24dcaec34afeacf5e1d01e68566f8f806e72a29
Nils Weißer <kontakt@nilsweisser.de>
2021-05-05 12:39:20 +0200

    Update README.md

---
+++ src/ApiKey.js
@@ -0,0 +1,4 @@
+export const api = {
+    key: "c37e07e694644ed483e19181818b7fa3",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-const api = {
-    key: "c37e07e694644ed483e19181818b7fa3",
-    base: "https://api.openweathermap.org/data/2.5/"
-}
+import {api} from "./ApiKey.js"
 
 function App() {
     const [query, setQuery] = useState('');
